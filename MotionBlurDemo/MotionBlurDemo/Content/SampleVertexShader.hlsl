0d1f5d90b51d28b4ece639816f184530d66b3883
RyanXu <77140485+HateKetchup@users.noreply.github.com>
2025-04-01 15:43:30 -0400

    Add texture coordinate support in vertex shader for pixel shader input

--- MotionBlurDemo/MotionBlurDemo/Content/SampleVertexShader.hlsl
+++ MotionBlurDemo/MotionBlurDemo/Content/SampleVertexShader.hlsl
@@ -9,12 +9,14 @@ struct VertexShaderInput
 {
 	float3 pos : POSITION;
 	float3 color : COLOR0;
+	float2 texCoord : TEXCOORD0;
 };
 
 struct PixelShaderInput
 {
 	float4 pos : SV_POSITION;
 	float3 color : COLOR0;
+	float2 texCoord : TEXCOORD0;
 };
 
 PixelShaderInput main(VertexShaderInput input)
@@ -28,6 +30,7 @@ PixelShaderInput main(VertexShaderInput input)
 	output.pos = pos;
 
 	output.color = input.color;
+	output.texCoord = input.texCoord; // Pass texture coordinates to the pixel shader.
 
 	return output;
 }
